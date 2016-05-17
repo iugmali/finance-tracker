d3336ae2faa560091f761c7802571e51b32454f2
Guilherme <contato@iugmali.com>
2016-05-17 18:33:46 +0000

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
   
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
   
-  devise_for :users, :controllers => { :registrations => "user/registrations" }
+  
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
